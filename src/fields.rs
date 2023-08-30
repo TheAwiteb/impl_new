6d2c04058c9b4f519a1c276ad00f7900019c0a59
TheAwiteb <awiteb@hotmail.com>
2023-08-31 01:42:13 +0300

    Init source commit

---
+++ src/fields.rs
@@ -0,0 +1,74 @@
+use proc_macro2::Ident;
+use proc_macro_error::abort;
+use syn::Type;
+
+use crate::attrs::Attrs;
+
+/// A field struct, which is a wrapper around a `String` and a `FieldType`.
+#[derive(Clone, Default)]
+pub(crate) struct Fields {
+    /// The names of the fields.
+    pub names: Vec<Ident>,
+    /// The types of the fields.
+    pub types: Vec<Type>,
+    /// The names of the values of the fields.
+    pub values: Vec<Ident>,
+    pub is_unnamed: bool,
+}
+
+impl Fields {
+    /// Parse the given derive input into a `Fields` struct.
+    pub(crate) fn parse(ast: &syn::DeriveInput) -> Self {
+        match ast.data {
+            syn::Data::Struct(ref data_struct) => match data_struct.fields {
+                syn::Fields::Named(ref fields_named) => Fields::from(fields_named),
+                syn::Fields::Unnamed(ref fields_unnamed) => Fields::from(fields_unnamed),
+                syn::Fields::Unit => {
+                    abort!(ast, "Unit structs are not supported for New derive macro");
+                }
+            },
+            _ => {
+                abort!(ast, "Only structs are supported for New derive macro");
+            }
+        }
+    }
+}
+
+impl From<&syn::FieldsNamed> for Fields {
+    fn from(fields_named: &syn::FieldsNamed) -> Self {
+        let mut fields = Fields::default();
+        for field in fields_named.named.iter() {
+            let attrs = Attrs::parse(&field.attrs);
+            let ident = field.ident.clone().unwrap();
+            fields.names.push(ident.clone());
+            fields
+                .values
+                .push(attrs.name.map(|(name, _)| name).unwrap_or(ident));
+            fields.types.push(field.ty.clone());
+        }
+        fields
+    }
+}
+
+impl From<&syn::FieldsUnnamed> for Fields {
+    fn from(fields_unnamed: &syn::FieldsUnnamed) -> Self {
+        let mut fields = Fields {
+            is_unnamed: true,
+            ..Fields::default()
+        };
+        for field in fields_unnamed.unnamed.iter() {
+            let attrs = Attrs::parse(&field.attrs);
+            if let Some(name) = attrs.name {
+                fields.values.push(name.0);
+                fields.types.push(field.ty.clone());
+            } else {
+                abort!(
+                    field,
+                    "Unnamed fields must have a name attribute.";
+                    help = "Write this after the field: `#[impl_new(name = \"name\")]`",
+                );
+            }
+        }
+        fields
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,126 @@
+#![doc = include_str!("../README.md")]
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+
+mod attrs;
+mod fields;
+
+fn impl_new(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
+    let struct_name = &ast.ident;
+    let new_function_doc = format!("Creates a new instance of [`{}`].", struct_name);
+    let fields = fields::Fields::parse(ast);
+    let names = fields.names;
+    let types = fields.types;
+    let values_names = fields.values;
+
+    if fields.is_unnamed {
+        quote! {
+            impl #struct_name {
+                #[doc = #new_function_doc]
+                pub fn new(
+                    #( #values_names: impl Into<#types> ),*
+                ) -> Self {
+                    Self(
+                        #( #values_names.into() ),*
+                    )
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl #struct_name {
+                #[doc = #new_function_doc]
+                pub fn new(
+                    #( #values_names: impl Into<#types> ),*
+                ) -> Self {
+                    Self {
+                        #( #names: #values_names.into() ),*
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// Derive macro that implements a new function for a struct.
+/// ## Attributes
+/// - `#[impl_new(name = "name")]`: Use this attribute to change the name of the argument in the generated `new` function.
+///
+//// ## Example
+/// ### For Named Fields
+///
+/// ```rust
+/// #[derive(impl_new::New)]
+/// struct Foo {
+///     name: String,
+///     age: usize,
+/// }
+///
+/// // The generated code will look like this:
+/// // impl Foo {
+/// //     pub fn new(name: impl Into<String>, age: Into<usize>) -> Self {
+/// //         Self { name: name.into(), age: age.into() }
+/// //     }
+/// // }
+///
+/// fn main() {
+///     let foo = Foo::new("Hello", 42usize); // Will use `Into::into` to convert the arguments to the fields types.
+///     assert_eq!(foo.name, "Hello".to_string());
+///     assert_eq!(foo.age, 42);
+/// }
+/// ```
+/// #### With Attributes
+///
+/// ```rust
+/// #[derive(impl_new::New)]
+/// struct Foo {
+///     #[impl_new(name = "user_name")]
+///     name: String,
+///     #[impl_new(name = "user_age")]
+///     age: usize,
+/// }
+///
+/// // The generated code will look like this:
+/// // impl Foo {
+/// //     pub fn new(user_name: impl Into<String>, user_age: Into<usize>) -> Self {
+/// //         Self { name: user_name.into(), age: user_age.into() }
+/// //     }
+/// // }
+///
+/// fn main() {
+///     let foo = Foo::new("Hello", 42usize); // Will use `Into::into` to convert the arguments to the fields types.
+///     assert_eq!(foo.name, "Hello".to_string());
+///     assert_eq!(foo.age, 42);
+/// }
+/// ```
+/// ### For Unnamed Fields
+///
+/// ```rust
+/// #[derive(impl_new::New)]
+/// struct Foo(#[impl_new(name = "name")] String, #[impl_new(name = "age")] usize);
+///
+/// // The generated code will look like this:
+/// // impl Foo {
+/// //     pub fn new(name: impl Into<String>, age: Into<usize>) -> Self {
+/// //         Self(name.into(), age.into())
+/// //     }
+/// // }
+///
+/// fn main() {
+///     let foo = Foo::new("Hello", 42usize); // Will use `Into::into` to convert the arguments to the fields types.
+///     assert_eq!(foo.0, "Hello".to_string());
+///     assert_eq!(foo.1, 42);
+/// }
+/// ```
+#[proc_macro_error::proc_macro_error]
+#[proc_macro_derive(New, attributes(impl_new))]
+pub fn new_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    let gen = impl_new(&ast);
+
+    gen.into()
+}
